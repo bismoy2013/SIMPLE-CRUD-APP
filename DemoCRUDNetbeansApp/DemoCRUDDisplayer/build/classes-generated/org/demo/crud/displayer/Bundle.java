365a0fa9983b96753aa6766c33d964822a23059c
bismoy2013 <bmurasing@gmail.com>
2014-07-09 15:26:52 +0530

    CRUD-APP

---
+++ DemoCRUDNetbeansApp/DemoCRUDDisplayer/build/classes-generated/org/demo/crud/displayer/Bundle.java
@@ -0,0 +1,27 @@
+package org.demo.crud.displayer;
+/** Localizable strings for {@link org.demo.crud.displayer}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>CRUDDisplayer</i>
+     * @see CRUDDisplayerTopComponent
+     */
+    static String CTL_CRUDDisplayerAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_CRUDDisplayerAction");
+    }
+    /**
+     * @return <i>CRUDDisplayer Window</i>
+     * @see CRUDDisplayerTopComponent
+     */
+    static String CTL_CRUDDisplayerTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_CRUDDisplayerTopComponent");
+    }
+    /**
+     * @return <i>This is a CRUDDisplayer window</i>
+     * @see CRUDDisplayerTopComponent
+     */
+    static String HINT_CRUDDisplayerTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_CRUDDisplayerTopComponent");
+    }
+    private void Bundle() {}
+}
